5affbecf29e4859d0d1b023372fd19b82abb9720
Robert <Bobgravity1@Roberts-MacBook-Pro-4.local>
2021-01-27 10:15:11 +0000

    new

--- frontend/src/screens/Productscreen.js
+++ frontend/src/screens/Productscreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import data from "../data";
 import Rating from "../components/Rating.js";
+import {Link} from 'react-router-dom'
 
 const Productscreen = (props) => {
   const product = data.products.find((x) => x._id === props.match.params.id);
@@ -8,12 +9,13 @@ const Productscreen = (props) => {
     return <div>product not found!</div>;
   }
   return (
-    <>
-      <div className="row">
+    <div>
+    <Link to='/'>Back to results</Link>
+      <div className="row top">
         <div className="col-2">
           <img className="large" src={product.image} alt={product.name} />
         </div>
-        <div className="col-1"></div>
+        <div className="col-1">
         <ul>
           <li>
             <h1>{product.name}</h1>
@@ -22,20 +24,22 @@ const Productscreen = (props) => {
             <Rating rating={product.rating} numReviews={product.numReviews} />
           </li>
           <li>price: {product.price}</li>
-          <li>description: {product.description}</li>
+          <li>description:
+          <p>{product.description}        </p>
+          </li>
         </ul>
+</div>
         <div className="col-1">
           <div className="card card-body">
             <ul>
               <li>
                 <div className="row">
-                  <div>Price:</div>
+                  <div>Price</div>
                   <div className="price">{product.price}</div>
                 </div>
               </li>
-
               <li>
-                <div className="row">
+                <div className="row row-align">
                   <div>status:</div>
                   <div>
                     {product.countInStock > 0 ? (
@@ -51,9 +55,9 @@ const Productscreen = (props) => {
               </li>
             </ul>
           </div>
-        </div>
+          </div>
+</div>
       </div>
-    </>
   );
 };
 
